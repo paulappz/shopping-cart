55b0315e6a29560d606942be494c8e45c5441fa0
paulappz <you@example.com>
2016-05-26 11:37:23 +0100

    directive for cart-table html file created

---
+++ app/tests/test_directives.js
@@ -0,0 +1,14 @@
+describe('<total-items> directive', function() {
+  beforeEach(module('shoppingCart'));
+
+});
+
+describe('<total-price> directive', function() {
+  beforeEach(module('shoppingCart'));
+
+});
+
+describe('<cart-table> directive', function() {
+  beforeEach(module('shoppingCart'));
+
+});
