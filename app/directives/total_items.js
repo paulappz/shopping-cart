d0b15ad8feda41d4e2fb573cbc4a393d003829fb
paulappz <you@example.com>
2016-05-26 11:28:09 +0100

    total-items directive file modified

--- app/directives/total_items.js
+++ app/directives/total_items.js
@@ -8,6 +8,15 @@ angular
 			scope: {
 		      cartItems: '=info'
 		    },
-			templateUrl: 'directives/total_items.html'
+			templateUrl: 'directives/total_items.html',
+			link: function(scope, element, attr) {
+				scope.getNumOfCartItems = function(cartItems) {
+					var numItems = 0
+					Object.keys(cartItems).forEach(function(i){
+						numItems += cartItems[i].qty;
+					});
+					return numItems;
+				}
+			}
 		};
 	});
