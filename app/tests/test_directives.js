011b48e2e5dcc782f2f4c45d00e852501f8e1fd1
paulappz <you@example.com>
2016-05-27 09:51:11 +0100

    test directive modified

--- app/tests/test_directives.js
+++ app/tests/test_directives.js
@@ -1,14 +1,57 @@
 describe('<total-items> directive', function() {
-  beforeEach(module('shoppingCart'));
+	
+	var $compile,
+      $rootScope,
+      $scope;
+
+	beforeEach(module('shoppingCart'));
+	
+  beforeEach(module('templates'));
+
+	beforeEach(inject(function(_$compile_, _$rootScope_){
+	    $compile = _$compile_;
+	    $rootScope = _$rootScope_;
+	}));
+
+	it('Replaces the element with the appropriate content', function() {
+    $rootScope.cartItems = {
+      0: {qty: 1},
+      1: {qty: 2}
+    };
+
+    var element = $compile('<total-items info="cartItems"></total-items>')($rootScope);
+    
+    $rootScope.$digest();
+    
+    // Check that the compiled element contains the templated content
+    expect(element.html()).toContain("Total number of items in your cart: ");
+    expect(element.html()).toContain("3");
+	});
+
+	it('isOutOfStock', function() {
+	});
+
+	it('putBackInStore', function() {
+	});
+
+  it('isOutOfStock', function() {
+	});
 
 });
 
 describe('<total-price> directive', function() {
-  beforeEach(module('shoppingCart'));
+	beforeEach(module('shoppingCart'));
+
+	it('calculates to price', function() {
+
+	});
 
 });
 
 describe('<cart-table> directive', function() {
-  beforeEach(module('shoppingCart'));
+	beforeEach(module('shoppingCart'));
 
+	it('displays cart table', function() {
+
+	});
 });
